99b9d08c21a53932898e50b2ff3997a5467e076b
최민호 <minho85zero@naver.com>
2026-01-01 15:09:45 +0900

    06-background-jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,20 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-      return prisma.user.findMany();
-    }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+      return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "minho85zero@gmail.com"
+      },
+    })
+
+    return { success: true, message: "Job queued" }
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
